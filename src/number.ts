f7a639c9bb26a4da2b3c348068918c9090f96111
jhuntoo <jonnolee84@gmai.com>
2017-01-26 13:33:35 +0000

    fix sumby

--- src/number.ts
+++ src/number.ts
@@ -3,13 +3,13 @@ import {BigNumber} from 'bignumber.js';
 export function sumBy<T>(array: T[], propGetter: (T) => BigNumber): BigNumber {
     const sum = array.reduce((a, b) => {
         return propGetter(a).plus(propGetter(b));
-    }, BigNumber(0));
+    }, new BigNumber(0));
     return sum;
 }
 
 export function sum(array: BigNumber[]): BigNumber {
     const sum  =array.reduce((a, b) => {
         return a.plus(b);
-    }, BigNumber(0));
+    }, new BigNumber(0));
     return sum;
 }
